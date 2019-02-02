5fb55df016b4f5fe95be67116bc2ea8a9fb0763d
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-02 13:58:19 +0000

    2maggiori

--- duemaggiori.c
+++ duemaggiori.c
@@ -4,17 +4,27 @@
 
 int main () 
 {
-    int valore, massimo = 0, contatore = 0, massimo2 = 0;
+    int valore, massimo, contatore = 0, massimo2, temporanea, valore2;
     printf("inserisci un valore \n");
-    while (contatore < 10) 
+    scanf("%d",&valore);
+    scanf("%d",&valore2);
+
+    massimo = valore; 
+    massimo2 = valore2;
+
+    while (contatore < 8) 
     {
         scanf("%d", &valore);
         contatore++;
-        if (valore > massimo)
+
+        if (massimo2 > massimo)
         {
+            temporanea = massimo2;
             massimo2 = massimo;
-            massimo = valore;
+            massimo = temporanea;
         }
+        if (valore > massimo)
+            massimo = valore;
     }
  
     printf("il massimo dei valori inseriti e' %d mentre il secondo massimo e' %d", massimo, massimo2);
