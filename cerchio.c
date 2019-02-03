407164a05ce4812896c87656b23f7fb5c78c597e
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-03 11:21:34 +0000

    raggio negativo

---
+++ cerchio.c
@@ -0,0 +1,20 @@
+/*calcolo del raggio del cerchio*/
+
+#include <stdio.h>
+
+int main () 
+{
+    double pi = 3.14, raggio;
+    
+    printf("inserisci il raggio che sia un valore positivo");
+    do 
+       scanf("%lf", &raggio);
+    while(raggio < 0.0);
+    
+    printf("il diametro e' %f \n", raggio * 2.0);
+    printf("la circonferenza e' %f \n", raggio * 2.0 * pi);
+    printf("la superficie e' %f \n", pi * raggio * raggio);
+    
+    return 0;
+
+}
