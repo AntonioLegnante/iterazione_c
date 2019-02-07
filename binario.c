00fdfedfb23d45cb4642f860889bb195e9998b49
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-07 11:56:35 +0000

    non completo

--- binario.c
+++ binario.c
@@ -4,37 +4,32 @@
 
 int main () 
 {
-    int binario, lavoro, sentinella = 1, cifre[100], i = 0, decimale, j, controllo;
+    int binario, controllobinario, sentinella = 1, bit, decimale = 0, potenze, momentanea;
     
     printf("inserisci un numero binario");
-    do 
+    do
     {
         scanf("%d", &binario);
-        lavoro = binario;
-        while (lavoro != 0 && sentinella)
+        momentanea = binario;
+        while (momentanea > 0 || sentinella == 0)
         {
-            controllo = lavoro % 10;
-            if (controllo != 1 && controllo != 0)
-                lavoro /= 10;
+            controllobinario = momentanea % 10;
+            if (controllobinario == 1 || controllobinario == 0)
+                momentanea /= 10;
             else 
                 sentinella = 0;
         }
-    }while(!sentinella);
+    }while(sentinella == 0);
     
+    potenze = 1;
+
     while(binario > 0)
     {
-        cifre[i] = binario % 10;
+        bit = binario % 10;
+        if (bit == 1)
+            decimale += potenze;
         binario /= 10;
-        i++;
-    }
-   
-    if (cifre[0] == 1)
-        decimale = 1;
-   
-    for (j = 1; j < i; j++)
-    {
-        if (cifre[i] == 1)
-            decimale += 2 * j;
+        potenze *= 2;
     }
 
     printf("la conversione e' %d", decimale);
