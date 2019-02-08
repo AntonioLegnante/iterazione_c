4debd7a86ee249c0bba72d0ef96d084dc53f1773
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-08 20:16:15 +0000

    ignora

---
+++ fattoriale.c
@@ -0,0 +1,30 @@
+/* fattoriale */
+
+int main ()
+{
+    int 
+    
+    for(i = 1; i <= 5; i++)
+    {
+        temp = i;
+
+        if (i - 1 != 0)
+        {
+            while(fattoriale - 1 != 0)
+            {
+                decremento = i--;
+                fattoriale *= decremento;
+            }
+        
+        else 
+            fattoriale = i;
+        
+            i = temp;
+
+            printf("%d", fattoriale);
+  
+     }
+  
+        return 0;
+
+}
