e22c056736b70d58fd8c65ab07b2a6cee530d1d3
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-09 13:01:27 +0000

    non completo

---
+++ rombo.c
@@ -0,0 +1,17 @@
+/* rappresentazione di un rombo */
+
+#include <stdio.h>
+
+int main ()
+{
+    printf("inserisci il lato del rombo che sia compreso tra 1 e 19");
+    do 
+    {
+        scanf("%d", &lato);
+    }while(lato < 1 || lato > 19 || lato % 2 == 0);
+
+    for (i = 1; i < lato; i++)
+        for (j = 1; j < lato; j++)
+            spazi = lato - 
+
+    
