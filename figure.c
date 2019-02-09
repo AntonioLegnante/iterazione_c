ecaf851785a430dedc8a75ee02806a889110aa54
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-09 12:38:39 +0000

    figure

---
+++ figure.c
@@ -0,0 +1,55 @@
+/* stampa di alcuni triangoli con i cicli for */
+
+#include <stdio.h>
+
+int main ()
+{
+    int i, j, z, spazi, caratteri;
+
+    for (i = 1; i <= 10; i++)
+    {
+        for (j = 1; j <= i; j++)
+            putchar('*');
+        printf("\n");
+    }
+
+    for (i = 10; i >= 1; i--)
+    {
+        for (j = i; j >= 1; j--)
+            putchar('*');
+        printf("\n");
+    }
+
+    for (i = 10; i >= 1; i--)
+    {
+        spazi = 10 - i;
+        caratteri = i;
+ 
+        for (j = 1; j <= spazi; j++)
+            putchar(' ');
+            for (z = 1; z <= caratteri; z++)
+                putchar('*'); 
+        printf("\n");
+        
+    }
+
+   
+    for (i = 1; i <= 10; i++)
+    {
+        spazi = 10 - i;
+        caratteri = i;
+ 
+        for (j = 1; j <= spazi; j++)
+            putchar(' ');
+            for (z = 1; z <= caratteri; z++)
+                putchar('*'); 
+        printf("\n");
+        
+    }
+
+
+     
+
+    return 0;
+
+}
