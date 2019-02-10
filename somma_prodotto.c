18cff25b23fd4b74ac3ae2d934574a28fe59a84d
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-10 20:37:42 +0000

    somma e prodotto

--- somma_prodotto.c
+++ somma_prodotto.c
@@ -6,13 +6,11 @@ int main ()
 {
     int i, prodotto = 1, somma = 0;
  
-    for (i = 1; i < 30; i++)
-    {
-        if (i % 2 == 1 && i <= 15)
-            prodotto *= i;
-        else 
-            somma += i;
-    }
+    for (i = 1; i <= 15; i += 2)
+        prodotto *= i;
+    
+    for (i = 2; i <= 30; i += 2)
+        somma += i;
    
     printf("la somma dei valori compresi tra 2 e 30 e' %d mentre il prodotto dei valori compresi tra 1 e 15 e' %d", somma, prodotto);
 
