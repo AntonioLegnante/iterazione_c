863f07704db6614877b685383a912341dd268ad3
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-10 21:18:10 +0000

    fatica

--- rombo.c
+++ rombo.c
@@ -4,14 +4,51 @@
 
 int main ()
 {
+   
+    int caratteri, j, i, z, spazi, lato;
+ 
     printf("inserisci il lato del rombo che sia compreso tra 1 e 19");
     do 
     {
         scanf("%d", &lato);
+    
     }while(lato < 1 || lato > 19 || lato % 2 == 0);
+   
+        caratteri = 1;
+   
+        for (i = 0; i <= lato / 2; i++)
+        {
+            spazi = lato / 2 - i;
+            
+            for (j = 0; j < spazi; j++)
+                putchar(' ');
+            
+            for (z = 0; z < caratteri; z++)
+                putchar('*');
+  
+           caratteri += 2;
 
-    for (i = 1; i < lato; i++)
-        for (j = 1; j < lato; j++)
-            spazi = lato - 
+           printf ("\n");
+       }
+    
+        caratteri = lato - 2;
+   
+        for (i = 1; i <= lato / 2; i++)
+        {
+            spazi = i;
+            
+            for (j = 0; j < spazi; j++)
+                putchar(' ');
+            
+            for (z = 0; z < caratteri; z++)
+                putchar('*');
+  
+           caratteri -= 2;
 
+           printf ("\n");
+       }
+
+       
+ return 0;
+}     
     
