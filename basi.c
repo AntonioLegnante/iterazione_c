40f02271a769760775136fa94517c312b718df83
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-02-27 19:20:41 +0100

    conversioni

--- basi.c
+++ basi.c
@@ -4,89 +4,74 @@
 
 int main ()
 {
-    int i, j, controllo, contatore = 0;
-    int bit[10];
-    int a[10];
-    char lettera;
-    char es[10];
+    int i, j, pesobin, pesoh, bit, temp, pesoexa;
+
 
     for (i = 0; i <= 255; i++)
     {
-        controllo = i;
-        printf("%d", i);
-        do 
+        pesobin = 128;
+        temp = i;
+        for (j = 0; j < 8; j++)
         {
-            bit[contatore] = controllo % 2;
-            controllo /= 2;
-            contatore++;
-        }while(controllo != 0);
-        
-        for (j = 0; j < 8 - contatore; j++)
-            printf("%d", 0);
-        for (j = contatore; j >= 0; j++)
-            printf("%d", bit[j]);
-     
-        contatore = 0;
-
-        controllo = i;
-        do
-        {
-            a[contatore] = controllo % 8;
-            controllo /= 8;
-            contatore++;
-        }while(controllo != 0);
-        
-        for (j = 0; j < 3 - contatore; j++)
-            printf("%d", 0);
-        for (j = contatore; j >= 0; j++)
-            printf("%d", a[j]);
- 
-        contatore = 0;
- 
-        controllo = i;
-        do 
-        {
-            lettera = (char) controllo % 16;
-            controllo /= 16;
-        switch(lettera)
-        {
-            case 10:
-            es[contatore] = 'A';
-            break;
-            
-            case 11:
-            es[contatore] = 'B';
-            break;
-
-            case 12:
-            es[contatore] = 'C';
-            break;
-   
-            case 13:
-            es[contatore] = 'D';
-            break;
-   
-            case 14:
-            es[contatore] = 'E';
-            break;
- 
-            case 15: 
-            es[contatore] = 'F';
-            break;
- 
-            default:
-            es[contatore] = lettera;
-            break;
+            bit = temp / pesobin;
+            temp -= bit * pesobin; 
+            printf("%d", bit);
+            pesobin = pesobin / 2;
         }
-            contatore++;
-        }while(controllo != 0);
+        printf("\t");
 
-       for (j = 0; j < 4 - contatore; j++)
-           printf("%d", 0);
-       for (j = contatore; j >= 0; j++)
-           printf("%c", es[j]);
+        pesoh = 64;
+        temp = i;
+        for (j = 0; j < 3; j++)
+        {
+            bit = temp / pesoh;
+            temp -= bit * pesoh; 
+            printf("%d", bit);
+            pesoh = pesoh / 8;
+        }
 
-       printf("\n");
+               printf("\t");
+
+        pesoexa = 16;
+        temp = i;
+        for (j = 0; j < 2; j++)
+        {
+            bit = temp / pesoexa;
+            switch(bit) 
+            {
+                case 10:
+                    printf("A");
+                break;
+
+                case 11:
+                    printf("B");
+                break;
+
+                case 12:
+                    printf("C");
+                break;
+
+                case 13:
+                    printf("D");
+                break;
+
+                case 14:
+                    printf("E");
+                break;
+
+                case 15:
+                    printf("F");
+                break;
+
+                default:
+                    printf("%d", bit);
+                break;
+            }
+
+            temp -= bit * pesoexa; 
+            pesoexa = pesoexa / 16;
+        }
+        printf("\n");
     }
        return 0;
 }
