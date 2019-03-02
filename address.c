86e99f5375cadf8a9aa0d291a61dddad9c6bc5b4
AntonioLegnante <AntonioLegnante12gmail.com>
2019-03-02 11:28:10 +0100

    puntatori generici

--- address.c
+++ address.c
@@ -10,8 +10,9 @@ int main ()
     for (i = 0; i < 10; i++)
     {
         indirizzo = &vett[i];
-        printf("%p\n", indirizzo);
+        printf("%p\n", (void *)indirizzo);
     }
     
     return 0;
 }
+/*puntatori generici trattati come puntatori void
